d9195931a7683e4813d7f57f361705cd7482b06f
Aqsa Mohsin <aqsa.mohsin@koderlabs.com>
2023-07-23 16:41:42 +0500

    changes

--- routes/vaccineRoutes.js
+++ routes/vaccineRoutes.js
@@ -1,16 +1,16 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 const {
-	createVaccine,
-	getVaccineById,
-	getVaccines,
-	changeVaccineStatus
-} = require("../controllers/vaccineController")
+  createVaccine,
+  getVaccineById,
+  getVaccines,
+  changeVaccineStatus,
+} = require("../controllers/vaccineController");
 
-router.post("/create", createVaccine)
-router.get("", getVaccines)
-router.get("/:id", getVaccineById)
-router.patch("/:id", changeVaccineStatus)
+router.post("/create", createVaccine);
+router.get("/", getVaccines);
+router.get("/:id", getVaccineById);
+router.patch("/:id", changeVaccineStatus);
 
-module.exports = router
+module.exports = router;
